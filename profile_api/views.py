0c1253b2a7f0713022e7ff318b02ea2fba627d11
Vineet Tomar <vineettomar056@gmail.com>
2020-04-19 00:59:42 +0530

    created a apiview for get,post,put,patch,delete

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profile_api import serializers
 
 class HelloApiView(APIView):
     """Test APi View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
         """Returns a list of api Views"""
@@ -15,3 +17,31 @@ class HelloApiView(APIView):
             'focused towards carrier'
         ]
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial update of object"""
+
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+
+        return Response({'method': 'DELETE'})
