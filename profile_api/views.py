adbffaf7cd65ade4dc43cd56ec8e07a1f9466923
Vineet Tomar <vineettomar056@gmail.com>
2020-04-19 00:12:15 +0530

    Added a Class apiview

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test APi View"""
+
+    def get(self,request,format=None):
+        """Returns a list of api Views"""
+
+        an_apiview = [
+            'Use to get all the views as a list',
+            'vineet tomar',
+            'keen to learning new things',
+            'focused towards carrier'
+        ]
+        return Response({'message':'Hello!','an_apiview':an_apiview})
